900c3f985701230e44aefa98f950803c412aa444
unknown <navneetdubeyit@gmail.com>
2018-06-15 01:11:58 +0530

    message

---
+++ src/main/java/ECOMMERCE/ECommerceApplication.java
@@ -0,0 +1,12 @@
+package ECOMMERCE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+}
